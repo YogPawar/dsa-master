4fd0ce6d40c768fe2a2a24da3c32ac888031fb01
Yogesh Pawar <pawaryog03@gmail.com>
2024-03-09 21:14:09 +0530

    refactored TwoSum methods to optimize the code

--- src/com/dsa/array/TwoSum.java
+++ src/com/dsa/array/TwoSum.java
@@ -1,12 +1,13 @@
 package com.dsa.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
   public static void main(String[] args) {
     TwoSum twoSum = new TwoSum();
-    System.out.println(Arrays.toString(twoSum.sum(new int[]{2, 7, 3, 6, 9, 2}, 11)));
+    System.out.println(Arrays.toString(twoSum.sumLogN(new int[]{2, 7, 3, 6, 9, 2}, 11)));
   }
 
   private int[] sum(int[] numbers, int target) {
@@ -19,5 +20,17 @@ public class TwoSum {
     }
     System.out.println("There is no combination found to achive target");
     return null;
+  }
+
+  private int[] sumLogN(int[] numbers, int target) {
+    HashMap<Integer, Integer> hm = new HashMap<>();
+    for (int i = 0; i < numbers.length; i++) {
+      if (hm.containsKey(target - numbers[i])) {
+        return new int[]{hm.get(target - numbers[i]), i};
+      } else {
+        hm.put(numbers[i], i);
+      }
+    }
+    return new int[]{-1, -1};
   }
 }
