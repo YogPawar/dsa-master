3056cc7150059073e2280c4ba36df1d78a62aed8
Yogesh Pawar <pawaryog03@gmail.com>
2024-03-11 21:11:17 +0530

    Roted 2D matrix

--- src/com/dsa/array/RotedMatrix.java
+++ src/com/dsa/array/RotedMatrix.java
@@ -27,6 +27,7 @@ public class RotedMatrix {
         matrix[first][i] = matrix[last - offset][first];
         matrix[last - offset][first] = matrix[last][last - offset];
         matrix[last][last - offset] = matrix[i][last];
+        matrix[i][last] = top;
       }
     }
     return true;
