c5c729f4a25da89cb96789cfe016657e32221a77
Yogesh Pawar <pawaryog03@gmail.com>
2024-03-17 16:22:13 +0530

    Created the LinkedList class for solving the problems

---
+++ src/com/dsa/linkedlist/questions/LinkedList.java
@@ -0,0 +1,50 @@
+package com.dsa.linkedlist.questions;
+
+public class LinkedList {
+
+  public Node head;
+  public Node tail;
+  public int size;
+
+  public void create(int value) {
+    Node newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    size++;
+  }
+
+  /**
+   * Single linked list
+   *
+   * @param value
+   */
+  public void insert(int value) {
+    Node newNode = new Node(value);
+    tail.next = newNode;
+    tail = newNode;
+    newNode.next = null;
+    size++;
+  }
+
+  public void traverse() {
+    Node runner = head;
+    while (runner != null) {
+      System.out.print(runner.data);
+      if (runner.next != null) {
+        System.out.print("->");
+      }
+      runner = runner.next;
+    }
+  }
+
+  public class Node {
+
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data) {
+      this.data = data;
+    }
+  }
+}
---
+++ src/com/dsa/linkedlist/questions/Main.java
@@ -0,0 +1,18 @@
+package com.dsa.linkedlist.questions;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    LinkedList list = new LinkedList();
+    list.create(1);
+    list.insert(2);
+    list.insert(3);
+    list.insert(4);
+    list.insert(5);
+    list.insert(6);
+
+    list.traverse();
+  }
+
+}
