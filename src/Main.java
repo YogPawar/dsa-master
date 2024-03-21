750aae27dfb0afd6ad8fd3a4f0d1f02ac5f2abe5
Yogesh Pawar <pawaryog03@gmail.com>
2024-03-21 21:35:17 +0530

    refactored the stack

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.LinkedList;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    LinkedList<Integer> list = new LinkedList<>();
+    list.add(10);
+    list.add(15);
+    list.add(16);
+    list.add(20);
+
+    System.out.println(list);
+    System.out.println(list.getFirst());
+    System.out.println(list.getLast());
+    list.remove(1);
+    System.out.println(list);
   }
 }
