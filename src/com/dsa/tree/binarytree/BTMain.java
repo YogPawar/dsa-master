6168614692ad82a44e357e8a21c21ea9616aa6b4
Yogesh Pawar <pawaryog03@gmail.com>
2024-08-30 23:05:46 +0530

    Added code for Tree and Binary Tree

--- src/com/dsa/tree/binarytree/BTMain.java
+++ src/com/dsa/tree/binarytree/BTMain.java
@@ -4,7 +4,30 @@ public class BTMain {
 
   public static void main(String[] args) {
     BinaryTree binaryTree = new BinaryTree();
+    binaryTree.insert("N1");
+    binaryTree.insert("N2");
+    binaryTree.insert("N3");
+    binaryTree.insert("N4");
+    binaryTree.insert("N5");
+    binaryTree.insert("N6");
+    binaryTree.insert("N7");
+    binaryTree.insert("N8");
+    binaryTree.insert("N9");
+    System.out.println("Pre Order : ");
+    binaryTree.preOrder(binaryTree.root);
+    System.out.println("In Order : ");
+    binaryTree.inOrder(binaryTree.root);
+    System.out.println("Post Order");
+    binaryTree.postOrder(binaryTree.root);
 
+    System.out.println();
+    System.out.println("Delete operation : ");
+    binaryTree.preOrder(binaryTree.root);
+    System.out.println("\nDeepest Node : " + binaryTree.getDeepestNode().data);
+    System.out.println();
+    binaryTree.deleteDeepestNode();
+    System.out.println("After delete");
+    binaryTree.preOrder(binaryTree.root);
   }
 
 }
--- src/com/dsa/tree/binarytree/BinaryTree.java
+++ src/com/dsa/tree/binarytree/BinaryTree.java
@@ -1,5 +1,8 @@
 package com.dsa.tree.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 
   BinaryTreeNode root;
@@ -32,7 +35,108 @@ public class BinaryTree {
     }
   }
 
-  public void insert(BinaryTreeNode root) {
+  public void levelOrder() {
+    Queue<BinaryTreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      BinaryTreeNode presentNode = queue.remove();
+      System.out.print(presentNode.data + " ");
+      if (presentNode.leftChild != null) {
+        queue.add(presentNode.leftChild);
+      }
 
+      if (presentNode.rightChild != null) {
+        queue.add(presentNode.rightChild);
+      }
+    }
+  }
+
+  public String search(String data) {
+    Queue<BinaryTreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      BinaryTreeNode presentNode = queue.remove();
+      if (presentNode.data.equals(data)) {
+        System.out.println("Value found in tree : " + presentNode.data);
+        return presentNode.data;
+      } else {
+
+        if (presentNode.leftChild != null) {
+          queue.add(presentNode.leftChild);
+        }
+        if (presentNode.rightChild != null) {
+          queue.add(presentNode.rightChild);
+        }
+      }
+    }
+    return null;
+  }
+
+  public void insert(String data) {
+    //create new node to add value
+    BinaryTreeNode newNode = new BinaryTreeNode();
+    newNode.data = data;
+    if (root == null) {
+      root = newNode;
+      System.out.println("Inserted at root.");
+      return;
+    }
+    //traverse the tree and insert at correct location (level order traversal).
+    Queue<BinaryTreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      BinaryTreeNode currentNode = queue.remove();
+      if (currentNode.leftChild == null) {
+        currentNode.leftChild = newNode;
+        System.out.println("Inserted at Left child.");
+        break;
+      } else if (currentNode.rightChild == null) {
+        currentNode.rightChild = newNode;
+        System.out.println("Inserted at right child.");
+
+        break;
+      } else {
+        queue.add(currentNode.leftChild);
+        queue.add(currentNode.rightChild);
+      }
+    }
+  }
+
+  public BinaryTreeNode getDeepestNode() {
+    BinaryTreeNode deepestNode = null;
+    Queue<BinaryTreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      deepestNode = queue.remove();
+      if (deepestNode.leftChild != null) {
+        queue.add(deepestNode.leftChild);
+      }
+      if (deepestNode.rightChild != null) {
+        queue.add(deepestNode.rightChild);
+      }
+    }
+    return deepestNode;
+  }
+
+  public void deleteDeepestNode() {
+    Queue<BinaryTreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    BinaryTreeNode previousNode, presentNode = null;
+    while (!queue.isEmpty()) {
+      previousNode = presentNode;
+      presentNode = queue.remove();
+
+      //check the deepest Node and remove the connection with previous node to deepest node.
+      if (presentNode.leftChild == null) {
+        previousNode.rightChild = null;
+        return;
+      } else if (presentNode.rightChild == null) {
+        previousNode.leftChild = null;
+        return;
+      } else {
+        queue.add(presentNode.leftChild);
+        queue.add(presentNode.rightChild);
+      }
+    }
   }
 }
